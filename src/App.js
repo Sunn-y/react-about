cc29a97a4717d7e87131a03ad462b1ab38795de6
Sunny Park <in.wise.nature@gmail.com>
2021-11-26 23:45:44 +0900

    [ui] default components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
-import "./scss/app.scss"
+import "./scss/styles.scss";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
+import Tree from "./assets/img/tree.png";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
       <div className="App">
         <Header />
         <main>
+          <img src={Tree} alt="tree.png" className="main-img"/>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="about" element={<About />} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,38 @@
-import "../scss/header.scss"
-import React from "react";
+import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 
-export default function Header() {
-  return (
-    <nav>
-      <Link to="/about">ABOUT</Link>
-      <Link to="/portfolio">PORTFOLIO</Link>
-      <Link to="/contact">CONTACT</Link>
-    </nav>
-  );
+export default class Header extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      path: '/'
+    }
+  }
+
+  route(next) {
+    this.setState({
+      path: next
+    })
+  }
+
+  render() {
+    const navItem = [
+      {path: "/about", name: "ABOUT"},
+      {path: "/portfolio", name: "PORTFOLIO"},
+      {path: "/contact", name: "CONTACT"}
+    ]
+    const navList = navItem.map(item =>
+      <Link
+        to={item.path}
+        key={item.name}
+        className={this.state.path === item.path ? 'active' : ''}
+        onClick={() => this.route(item.path)}>
+          {item.name}
+      </Link>
+    )   
+    return (
+    <nav>{navList}</nav>
+    )
+  }
 }
